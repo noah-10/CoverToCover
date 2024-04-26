c4358ba5977ed8b629ce9d017548552fcdcab976
Spencer Surface <54449845+SpencerSurface@users.noreply.github.com>
2024-04-25 20:07:09 -0400

    add react router provider in main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,34 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
 import App from './App.jsx'
-import './index.css'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <NotFound />,
+    children: [
+      {
+        index: true,
+        element: <Home />
+      },
+      {
+        path: '/feed',
+        element: <Feed />
+      },
+      {
+        path: '/library',
+        element: <Library />
+      },
+      {
+        path: '/settings',
+        element: <Settings />
+      }
+    ]
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <RouterProvider router={router} />
 )
