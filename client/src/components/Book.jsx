5ce38a473656ea832020d149f842faa39cd5f991
noah-10 <noah.fryman.10@gmail.com>
2024-04-28 00:53:06 -0400

    Addde slight styling to library: added flex-direction row for books and an overflow option

--- client/src/components/Book.jsx
+++ client/src/components/Book.jsx
@@ -5,12 +5,6 @@ const Book = (props) => {
             <div className="saved-cover">
                 <img src={props.cover} alt={`image of book cover for ${props.title}`} />
             </div>
-            <div className="saved-title">
-                <p>{props.title}</p>
-            </div>
-            <div className="saved-author">
-                <p>{props.author}</p>
-            </div>
         </div>
     )
 }
