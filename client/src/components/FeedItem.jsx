2208f453aabef4d8740ee962afedf6c6353a1792
Spencer Surface <54449845+SpencerSurface@users.noreply.github.com>
2024-05-01 17:29:42 -0400

    add handler to let save book button save books for a user

--- client/src/components/FeedItem.jsx
+++ client/src/components/FeedItem.jsx
@@ -1,11 +1,28 @@
+import { useMutation } from "@apollo/client";
+import { SAVE_BOOK } from "../../utils/mutations";
+
 const FeedItem = ({ feedItem, incrementFeed } ) => {
+    const [saveBook] = useMutation(SAVE_BOOK);
+
+    const handleSaveClick = async () => {
+        try {
+            await saveBook({ variables: { input: {
+                ...feedItem,
+                firstSentence: feedItem.firstSentence ? feedItem.firstSentence[0] : ""
+            } } });
+        } catch (error) {
+            console.error(error);
+        }
+        incrementFeed();
+    }
+
     return (
         <>
             <img style={{width: "25vw"}} src={feedItem.cover} alt={`Cover of the book "${feedItem.title}`}></img>
             <div>Title: {feedItem.title}</div>
             <div>Authors: {feedItem.authors}</div>
             <button onClick={() => incrementFeed()}>Dismiss Book</button>
-            <button onClick={() => incrementFeed()}>Save Book</button>
+            <button onClick={() => handleSaveClick()}>Save Book</button>
         </>
     )
 }
