00f782c0ac515944198303eb9686b3428c5e499a
Spencer Surface <54449845+SpencerSurface@users.noreply.github.com>
2024-05-01 21:05:38 -0400

    if no first sentence, say so in book modal

--- client/src/components/BookModal.jsx
+++ client/src/components/BookModal.jsx
@@ -14,7 +14,7 @@ const BookModal = ({ closeModal, book }) => {
                     <h1>{book.authors}</h1>
                 </div>
                 <div className="body">
-                    <p>description...</p>
+                    <p>{book.firstSentence ? book.firstSentence : "No preview available"}</p>
                 </div>
                 <div className="footer">
                     <button onClick={() => closeModal()}>Close</button>
